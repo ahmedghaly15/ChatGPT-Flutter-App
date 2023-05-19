670d270143b38c05d911bb307c8f3ea6858d8aed
ahmed ghaly <ahmedghaly0767@gmail.com>
2023-05-20 02:28:41 +0300

    Little modifications

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,10 @@ import 'package:chatgpt_flutter_app/core/global/app_theme.dart';
 import 'package:chatgpt_flutter_app/features/chat/presentation/view/chat_view.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const ChatGPTFlutterApp());
-}
+void main() => runApp(const ChatGPTUI());
 
-// TODO: don't forget at the end to setup the fontFamily
-
-class ChatGPTFlutterApp extends StatelessWidget {
-  const ChatGPTFlutterApp({super.key});
+class ChatGPTUI extends StatelessWidget {
+  const ChatGPTUI({super.key});
 
   @override
   Widget build(BuildContext context) {
